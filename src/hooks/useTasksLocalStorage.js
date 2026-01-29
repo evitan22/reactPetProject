f901448973ac4eb28bcd86b7e69ee39edf03ec30
evitan22 <kokravets@gmail.com>
2026-01-30 00:44:35 +0200

    made lesson 12

---
+++ src/hooks/useTasksLocalStorage.js
@@ -0,0 +1,14 @@
+const useTaskLocalSrorage = () => {
+    const savedTasks = localStorage.getItem('tasks')
+
+    const saveTasks = (tasks) => {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+    return {
+        savedTasks: savedTasks ? JSON.parse(savedTasks) : null,
+        saveTasks,
+    }
+}
+
+export default useTaskLocalSrorage
